8665356f3bbc881b83eefe3ae3ed8de3b80a190a
calebkkiatoukaysi-ksu <calebkkiatoukaysi@ksu.edu>
2026-01-28 18:04:52 -0600

    Form created, needs logic

--- Kanaban501app/Kanaban501app/Form1.Designer.cs
+++ Kanaban501app/Kanaban501app/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Kanaban501app
 {
-    partial class Form1
+    partial class Kanban501
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Kanban501Label = new System.Windows.Forms.Label();
+            this.WorkingOnLabel = new System.Windows.Forms.Label();
+            this.ToDoLabel = new System.Windows.Forms.Label();
+            this.DoneLabel = new System.Windows.Forms.Label();
+            this.ToDoList = new System.Windows.Forms.ListBox();
+            this.WorkingOnList = new System.Windows.Forms.ListBox();
+            this.DoneList = new System.Windows.Forms.ListBox();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Kanban501Label
+            // 
+            this.Kanban501Label.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Kanban501Label.AutoSize = true;
+            this.Kanban501Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Kanban501Label.Location = new System.Drawing.Point(610, 75);
+            this.Kanban501Label.Name = "Kanban501Label";
+            this.Kanban501Label.Size = new System.Drawing.Size(372, 73);
+            this.Kanban501Label.TabIndex = 0;
+            this.Kanban501Label.Text = "Kanban501";
+            this.Kanban501Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WorkingOnLabel
+            // 
+            this.WorkingOnLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.WorkingOnLabel.AutoSize = true;
+            this.WorkingOnLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WorkingOnLabel.Location = new System.Drawing.Point(662, 219);
+            this.WorkingOnLabel.Name = "WorkingOnLabel";
+            this.WorkingOnLabel.Size = new System.Drawing.Size(225, 42);
+            this.WorkingOnLabel.TabIndex = 1;
+            this.WorkingOnLabel.Text = "Working On";
+            // 
+            // ToDoLabel
+            // 
+            this.ToDoLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ToDoLabel.AutoSize = true;
+            this.ToDoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ToDoLabel.Location = new System.Drawing.Point(224, 219);
+            this.ToDoLabel.Name = "ToDoLabel";
+            this.ToDoLabel.Size = new System.Drawing.Size(125, 42);
+            this.ToDoLabel.TabIndex = 2;
+            this.ToDoLabel.Text = "To Do";
+            // 
+            // DoneLabel
+            // 
+            this.DoneLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DoneLabel.AutoSize = true;
+            this.DoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DoneLabel.Location = new System.Drawing.Point(1175, 219);
+            this.DoneLabel.Name = "DoneLabel";
+            this.DoneLabel.Size = new System.Drawing.Size(112, 42);
+            this.DoneLabel.TabIndex = 3;
+            this.DoneLabel.Text = "Done";
+            // 
+            // ToDoList
+            // 
+            this.ToDoList.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.ToDoList.FormattingEnabled = true;
+            this.ToDoList.ItemHeight = 25;
+            this.ToDoList.Location = new System.Drawing.Point(98, 306);
+            this.ToDoList.Name = "ToDoList";
+            this.ToDoList.Size = new System.Drawing.Size(383, 404);
+            this.ToDoList.TabIndex = 4;
+            // 
+            // WorkingOnList
+            // 
+            this.WorkingOnList.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.WorkingOnList.FormattingEnabled = true;
+            this.WorkingOnList.ItemHeight = 25;
+            this.WorkingOnList.Location = new System.Drawing.Point(578, 306);
+            this.WorkingOnList.Name = "WorkingOnList";
+            this.WorkingOnList.Size = new System.Drawing.Size(383, 404);
+            this.WorkingOnList.TabIndex = 5;
+            // 
+            // DoneList
+            // 
+            this.DoneList.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DoneList.FormattingEnabled = true;
+            this.DoneList.ItemHeight = 25;
+            this.DoneList.Location = new System.Drawing.Point(1044, 306);
+            this.DoneList.Name = "DoneList";
+            this.DoneList.Size = new System.Drawing.Size(383, 404);
+            this.DoneList.TabIndex = 6;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.NewButton.Location = new System.Drawing.Point(98, 759);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(181, 61);
+            this.NewButton.TabIndex = 7;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            // 
+            // EditButton
+            // 
+            this.EditButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.EditButton.Location = new System.Drawing.Point(300, 759);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(181, 61);
+            this.EditButton.TabIndex = 8;
+            this.EditButton.Text = "Edit";
+            this.EditButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.DeleteButton.Location = new System.Drawing.Point(1246, 749);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(181, 61);
+            this.DeleteButton.TabIndex = 9;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // Kanban501
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1566, 869);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.NewButton);
+            this.Controls.Add(this.DoneList);
+            this.Controls.Add(this.WorkingOnList);
+            this.Controls.Add(this.ToDoList);
+            this.Controls.Add(this.DoneLabel);
+            this.Controls.Add(this.ToDoLabel);
+            this.Controls.Add(this.WorkingOnLabel);
+            this.Controls.Add(this.Kanban501Label);
+            this.Name = "Kanban501";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Kanban501";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Kanban501Label;
+        private System.Windows.Forms.Label WorkingOnLabel;
+        private System.Windows.Forms.Label ToDoLabel;
+        private System.Windows.Forms.Label DoneLabel;
+        private System.Windows.Forms.ListBox ToDoList;
+        private System.Windows.Forms.ListBox WorkingOnList;
+        private System.Windows.Forms.ListBox DoneList;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.Button DeleteButton;
     }
 }
 
--- Kanaban501app/Kanaban501app/Form1.cs
+++ Kanaban501app/Kanaban501app/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Kanaban501app
 {
-    public partial class Form1 : Form
+    public partial class Kanban501 : Form
     {
-        public Form1()
+        public Kanban501()
         {
             InitializeComponent();
         }
